e257dce9890d255f0222fd1f58947b1bc8f1d4d5
basilserpent <basiliskboy17@gmail.com>
2020-03-30 19:51:43 +0200

    Add files via upload

--- src/main/java/com/fossilbeach/fossilbeach/FossilBeach.java
+++ src/main/java/com/fossilbeach/fossilbeach/FossilBeach.java
@@ -3,17 +3,16 @@
  * Comment on different functions, chances are you'll forget what they do
  */
 package com.fossilbeach.fossilbeach;
-import com.fossilbeach.fossilbeach.blocks.BlockRegistry;
-import com.fossilbeach.fossilbeach.items.ItemRegistry;
+import com.fossilbeach.fossilbeach.blocks.Blocks;
+import com.fossilbeach.fossilbeach.items.Items;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(FossilBeach.MOD_ID)
 public class FossilBeach {
     public FossilBeach(){
-        ItemRegistry.BLOCKITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BlockRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        Items.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        Blocks.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
 public static final String MOD_ID = "fossilbeach";
